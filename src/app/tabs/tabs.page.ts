08aa99dd98cb86d88c75205d0eef52e4c80057b5
ScottCatton <78232367+MapleSerup@users.noreply.github.com>
2025-08-27 21:13:09 -0400

    Creating the mobile application

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,20 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import {
+  triangle,
+  ellipse,
+  square,
+  home,
+  barChart,
+  barbell,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -13,6 +26,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({ triangle, ellipse, square, home, barChart, barbell });
   }
 }
